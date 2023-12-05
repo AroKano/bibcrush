98c0f063f0592b7a0b23e6e1a8adfb1bdd169462
hilal09 <hilal.cubukcu02@gmail.com>
2023-12-06 00:19:37 +0100

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bibcrush/pages/login_page.dart';
 import 'package:bibcrush/pages/profile_page.dart';
 import 'package:flutter/material.dart';
 
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart'; //import von packages
 
 //Main Funktion, ruft runApp Funktion auf, übergibt MyApp als Hauptwidget
-void main() {
-  runApp(MyApp());
-}
-//Klasse definiert, erbt von StatelessWidget ok
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp( //MyApp gint MaterialApp zurück, hat als ahuptwidget die LoginPage
-      home: LoginPage(),
-    );
-  }
-}
+// void main() {
+//   runApp(MyApp());
+// }
+// //Klasse definiert, erbt von StatelessWidget ok
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp( //MyApp gint MaterialApp zurück, hat als ahuptwidget die LoginPage
+//       home: LoginPage(),
+//     );
+//   }
+// }
+
 //StatefulWidget (zustand derseite kann sich ändern)
 class LoginPage extends StatefulWidget {
+  const LoginPage({super.key}); //damit man das in der main file aufrufen kann
+
   @override
-  _LoginPageState createState() => _LoginPageState(); //erstellt zugehöriges state-objekt, verwaltet zustand der seite
+  _LoginPageState createState() =>
+      _LoginPageState(); //erstellt zugehöriges state-objekt, verwaltet zustand der seite
 }
+
 //zustand für loginPage wird definiert
 class _LoginPageState extends State<LoginPage> {
   bool isAnmeldenTab = true; //damit kann man den ausgewählten tab verfolgen
@@ -25,23 +30,27 @@ class _LoginPageState extends State<LoginPage> {
   //build methode, erstellt ui der seite
   @override
   Widget build(BuildContext context) {
-    return Scaffold( //grundgerüst???
-      appBar: AppBar( //app leiste
-        title: const
-        Text(
+    return Scaffold(
+      //grundgerüst???
+      appBar: AppBar(
+        //app leiste
+        title: const Text(
           'BibCrush', //titel code card
           style: TextStyle(color: Colors.black), //weisser text
         ),
         backgroundColor: Colors.white, //hintergrundfarbe
         elevation: 0,
       ),
-      body: Container( //body container, hauptinhalt der seite
+      body: Container(
+        //body container, hauptinhalt der seite
         color: Colors.white, //hintergrundfarbe
         padding: const EdgeInsets.all(16.0), //abstand zwischen ui elementen
-        child: Column( //widget wird verwendet um widgets vertikal anzuordnen
+        child: Column(
+          //widget wird verwendet um widgets vertikal anzuordnen
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Row( //buttons für anmelden und registrieren
+            Row(
+              //buttons für anmelden und registrieren
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 buildTabButton('Anmelden', true),
@@ -49,8 +58,10 @@ class _LoginPageState extends State<LoginPage> {
               ],
             ),
             const SizedBox(height: 20),
-            TextField( //textfeld für email
-              style: const TextStyle(color: Colors.grey), // Textfarbe weiß setzen
+            TextField(
+              //textfeld für email
+              style:
+                  const TextStyle(color: Colors.grey), // Textfarbe weiß setzen
               decoration: InputDecoration(
                 labelText: 'E-mail Adresse',
                 prefixIcon: const Icon(Icons.mail, color: Colors.grey),
@@ -66,8 +77,10 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ),
             const SizedBox(height: 20),
-            TextField( //textfeld für passwort
-              style: const TextStyle(color: Colors.grey), // Textfarbe weiß setzen
+            TextField(
+              //textfeld für passwort
+              style:
+                  const TextStyle(color: Colors.grey), // Textfarbe weiß setzen
               decoration: InputDecoration(
                 labelText: 'Passwort',
                 prefixIcon: const Icon(Icons.lock, color: Colors.grey),
@@ -84,12 +97,15 @@ class _LoginPageState extends State<LoginPage> {
               obscureText: true, //passwort verbergen
             ),
             const SizedBox(height: 20),
-            ElevatedButton( //erhebener button für bestätigung
-              onPressed: () { //
+            ElevatedButton(
+              //erhebener button für bestätigung
+              onPressed: () {
+                //
                 // ANMELDELOGIK FEHLT!!! FIREBASE!!!
                 print('Bestätigen erfolgreich');
               },
-              style: ElevatedButton.styleFrom( //button stil
+              style: ElevatedButton.styleFrom(
+                //button stil
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
                 ),
@@ -111,15 +127,18 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  Widget buildTabButton(String text, bool isSelected) { //funktion für tab buttons
-    return ElevatedButton( //tab wird hervorgehoben
+  Widget buildTabButton(String text, bool isSelected) {
+    //funktion für tab buttons
+    return ElevatedButton(
+      //tab wird hervorgehoben
       onPressed: () {
         setState(() {
           isAnmeldenTab = isSelected;
         });
       },
       style: ElevatedButton.styleFrom(
-        backgroundColor : isSelected ? const Color(0xFFFF7A00) : const Color(0xFFFF7A00),
+        backgroundColor:
+            isSelected ? const Color(0xFFFF7A00) : const Color(0xFFFF7A00),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(10),
         ),
