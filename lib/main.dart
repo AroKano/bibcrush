b3cf1735f9ba6d0054d0110848d964e61814321e
hilal09 <hilal.cubukcu02@gmail.com>
2023-12-08 15:38:24 +0100

    iwas

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:bibcrush/pages/opening_page.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      // options: DefaultFirebaseOptions.currentPlatform,
+// options: DefaultFirebaseOptions.currentPlatform,
       );
 
   runApp(const MyApp());
