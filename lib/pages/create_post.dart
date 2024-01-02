b16be18c458f34edbe3511225bf2ce04678a74c8
Melisa <melisa.rosic1@gmail.com>
2024-01-02 02:07:26 +0100

    Create_post changes and functionality
    Changes in:
    - create_post
    - pubspec.yaml
    - info.plist
    - androidmanifest.xml

--- lib/pages/create_post.dart
+++ lib/pages/create_post.dart
@@ -1,132 +1,156 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class CreatePostPage extends StatelessWidget {
+void main() {
+  runApp(CreatePost());
+}
+
+class CreatePost extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(16.0, 32.0, 16.0, 16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.clear,
-                      color: Color(0xFFFF7A00)), // Akzentfarbe
-                  onPressed: () {
-                    // Hier kannst du die Logik für das Schließen implementieren
-                    print('Cancel');
-                  },
+    return MaterialApp(
+      title: 'Create Post',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: CreatePostPage(),
+    );
+  }
+}
+
+class CreatePostPage extends StatefulWidget {
+  @override
+  _CreatePostPageState createState() => _CreatePostPageState();
+}
+
+class _CreatePostPageState extends State<CreatePostPage> {
+  XFile? _image;
+
+  void _captureImage() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image = await _picker.pickImage(source: ImageSource.camera);
+
+    if (image != null) {
+      setState(() {
+        _image = image;
+      });
+    }
+  }
+
+  void _uploadMedia() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? file = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (file != null) {
+      setState(() {
+        _image = file;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          leading: OverflowBox(
+            maxWidth: double.infinity,
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: EdgeInsets.only(left: 15.0),
+              child: TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.zero,
+                  foregroundColor: Color(0xFFE85555),
                 ),
-                Text(
-                  'New Post',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(Icons.check,
-                      color: Color(0xFFFF7A00)), // Akzentfarbe
-                  onPressed: () {
-                    // Hier kannst du die Logik für das Bestätigen implementieren
-                    print('Post');
-                  },
-                ),
-              ],
-            ),
-            SizedBox(height: 16.0),
-            Container(
-              height: 200.0,
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.primary,
-                borderRadius: BorderRadius.circular(10.0),
+                child: Text('Cancel', style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
               ),
-              padding: EdgeInsets.all(8.0),
-              child: Theme(
-                data: Theme.of(context).copyWith(
-                  textSelectionTheme: TextSelectionThemeData(
-                    cursorColor: Color(0xFFFF7A00), // Akzentfarbe
-                    selectionColor: Color(0xFFFF7A00), // Akzentfarbe
-                    selectionHandleColor: Color(0xFFFF7A00), // Akzentfarbe
-                  ),
-                ),
+            ),
+          ),
+          title: Text(''),
+          actions: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(right: 10.0),
+              child: TextButton(
+                onPressed: () {
+                  // Handle post action
+                },
+                child: Text('Post', style: TextStyle(fontSize: 18.0, color: Color(0xFFFF7A00), fontWeight: FontWeight.bold)),
+              ),
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              if (_image != null) Image.file(File(_image!.path)),
+              Divider(
+                height: 1,
+                color: Color(0xFFE7E7E7),
+              ),
+              Padding(
+                padding: EdgeInsets.all(12.0),
                 child: TextField(
-                  cursorColor: Color(0xFFFF7A00), // Akzentfarbe
-                  style: TextStyle(color: Colors.black),
-                  maxLines: null,
                   decoration: InputDecoration(
+                    hintText: 'Type something...',
+                    hintStyle: TextStyle(fontSize: 18.0, color: Color(0xFF939393), fontWeight: FontWeight.normal),
                     border: InputBorder.none,
-                    hintText: 'Do you have a crush?',
-                    hintStyle: TextStyle(color: Colors.grey),
+                    contentPadding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
                   ),
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
                 ),
               ),
-            ),
-            SizedBox(height: 16.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.4,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Hier kannst du die Logik für die Kamera implementieren
-                      print('Capture');
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                          Color(0xFFFF7A00)), // Akzentfarbe
-                      foregroundColor: MaterialStateProperty.all(Colors.white),
-                      shape: MaterialStateProperty.all(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                      ),
-                    ),
-                    child: Container(
-                      alignment: Alignment.center,
-                      child: Column(
-                        children: [
-                          Icon(Icons.camera_alt),
-                          Text('Capture'),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.4,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Hier kannst du die Logik für das Foto hochladen implementieren
-                      print('Upload');
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                          Color(0xFFFF7A00)), // Akzentfarbe
-                      foregroundColor: MaterialStateProperty.all(Colors.white),
-                      shape: MaterialStateProperty.all(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                      ),
-                    ),
-                    child: Container(
-                      alignment: Alignment.center,
-                      child: Column(
-                        children: [
-                          Icon(Icons.file_upload),
-                          Text('Upload'),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              // ... Rest of the widgets ...
+            ],
+          ),
+        ),
+        bottomNavigationBar: Container(
+          padding: EdgeInsets.symmetric(horizontal: 30.0),
+          color: Theme.of(context).colorScheme.background,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Expanded(
+                child: _buildActionItem(context, 'Capture', Icons.camera_alt, Color(0xFFC6D2DD), Color(0xFF41698D), _captureImage),
+              ),
+              SizedBox(width: 12),
+              Expanded(
+                child: _buildActionItem(context, 'Upload', Icons.file_upload, Color(0xFFD6ECCF), Color(0xFF78C05F), _uploadMedia),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildActionItem(BuildContext context, String text, IconData icon, Color color, Color iconColor, VoidCallback onPressed) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: color,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          fixedSize: Size.fromHeight(60),
+        ),
+        onPressed: onPressed,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(icon, color: iconColor),
+            SizedBox(width: 8),
+            Text(text, style: TextStyle(color: Color(0xFF323232), fontWeight: FontWeight.bold)),
           ],
         ),
       ),
